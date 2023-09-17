da49ecb51048a07d0c39e52560d646f1001b31d7
nuwan <nuwansamarasinghe100@gmail.com>
2023-09-17 18:36:33 +0100

    adding the initial code changes

---
+++ src/test/java/com/demo/springbootdemoapi/SpringBootDemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootdemoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
