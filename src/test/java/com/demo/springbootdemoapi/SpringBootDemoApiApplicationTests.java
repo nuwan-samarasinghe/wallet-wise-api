d982f909b115642a5b7d4968c600d44b6f180a6e
nuwan <nuwansamarasinghe100@gmail.com>
2023-09-17 20:07:10 +0100

    adding liquibase to manage sql changelog.yml

--- src/test/java/com/demo/springbootdemoapi/SpringBootDemoApiApplicationTests.java
+++ src/test/java/com/demo/springbootdemoapi/SpringBootDemoApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootDemoApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
